8ad83802cc44d7015289f35756b71903337c2c4e
mattraydev <cedaring.xr@gmail.com>
2023-12-16 18:44:43 -0700

    minor additions to flashcards component

--- src/components/basic/Flashcards.tsx
+++ src/components/basic/Flashcards.tsx
@@ -8,8 +8,9 @@ export default function Flashcards() {
 	const [showAnswer, setShowAnswer] = useState(false)
 	const [activeCard, setActiveCard] = useState(0) //loop through array of cards
 	const [form, setForm] = useState({ question: '', answer: '' })
+	const [cardList, setCardList] = useState([])
 
-	const cardList = []
+	const flashcardList = []
 
 	const handleCardChange = (e: any): void => {
 		setForm({
@@ -21,19 +22,34 @@ export default function Flashcards() {
 	const createNewCard = () => {}
 
 	return (
-		<section className="component">
+		<section className="component h-[600px]">
 			<h2 className="component-title">Simple flashcards</h2>
-			<div>
+			<div className="w-[300px]">
 				<p>Create new Card</p>
-				<form action="">
+				<form action="" className="">
 					<input type="text" name="question" className="textInput" onChange={handleCardChange} />
 					<input type="text" name="answer" className="textInput" onChange={handleCardChange} />
 				</form>
-				<button className="button" onClick={createNewCard}></button>
+				<button className="button" onClick={createNewCard}>
+					Add New Flashcard
+				</button>
 			</div>
-			<div id="cards-container">
-				<div id="card"></div>
+			<div id="cards-container" className="relative">
+				<Flashcard cardFront="Question for Flash card" cardBack="Answer to Flash card" />
 			</div>
 		</section>
+	)
+}
+
+const Flashcard = ({ cardFront, cardBack }: any) => {
+	return (
+		<div id="card" className="card">
+			<p className="card-front">
+				<h4 className="text-center my-8">{cardFront}</h4>
+			</p>
+			<p className="card-back">
+				<h4 className="text-center my-8">{cardBack}</h4>
+			</p>
+		</div>
 	)
 }
